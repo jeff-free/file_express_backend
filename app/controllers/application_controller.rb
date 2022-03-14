2d6076544be4470f21ee0e1725f1e347aa60b783
JFree <jeffrey410469@gmail.com>
2022-03-14 23:20:59 +0800

    - Add `app/models/sharing.rb` - Add api endpoint `POST /sharings` to create sharings with pre-direct-uploaded files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+  include Rendering
+
+  rescue_from ActiveRecord::RecordNotFound, with: :respond_record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :respond_unprocessable_entity
+  rescue_from ActiveSupport::MessageVerifier::InvalidSignature, with: :respond_unprocessable_entity
+
+  def respond_bad_request(exception)
+    message = get_exception_message(exception, HTTP_MESSAGE_BADREQUEST)
+    render json: get_result_json(HTTP_CODE_BAD_REQUEST, message),
+           status: HTTP_CODE_BAD_REQUEST
+  end
+
+  def respond_unprocessable_entity(exception)
+    message = get_exception_message(exception, HTTP_MESSAGE_UNPROCESSABLEENTITY)
+    render json: get_result_json(HTTP_CODE_UNPROCESSABLEENTITY, message),
+           status: HTTP_CODE_UNPROCESSABLEENTITY
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_12_105531) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_13_133326) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_12_105531) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "sharings", force: :cascade do |t|
+    t.string "token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["token"], name: "index_sharings_on_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
